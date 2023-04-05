e36c0846c28454122a22652ff3bb4dda651eeba3
Kirill Yakimchuk <k.yakimchuk@ventra.ru>
2023-04-05 20:22:26 +0300

    Add functions for working with order.

--- src/main/kotlin/com/rafeng/bakery/improve/business/repository/OrderRepository.kt
+++ src/main/kotlin/com/rafeng/bakery/improve/business/repository/OrderRepository.kt
@@ -1,10 +1,12 @@
 package com.rafeng.bakery.improve.business.repository
 
 import com.rafeng.bakery.improve.business.model.dto.Order
+import org.springframework.stereotype.Component
 
 /**
  * This class describes orders that has been done so far
  */
+@Component
 class OrderRepository (
     private val orders: MutableList<Order> = mutableListOf()
 ) {
