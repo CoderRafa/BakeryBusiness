9ce7d4f34d240126b060f7afd230d6270cd6116e
Kirill Yakimchuk <k.yakimchuk@ventra.ru>
2023-05-07 12:03:18 +0300

    Fix a test.

--- src/main/kotlin/com/rafeng/bakery/improve/business/controller/RecipeController.kt
+++ src/main/kotlin/com/rafeng/bakery/improve/business/controller/RecipeController.kt
@@ -3,13 +3,14 @@ package com.rafeng.bakery.improve.business.controller
 import com.rafeng.bakery.improve.business.model.dto.Recipe
 import com.rafeng.bakery.improve.business.service.impl.RecipeService
 import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/recipe")
+@RequestMapping("/api/v1/recipe", consumes = [MediaType.APPLICATION_JSON_VALUE])
 class RecipeController(private val recipeService: RecipeService) {
     private val log = LoggerFactory.getLogger(RecipeController::class.java)
 
--- src/test/kotlin/com/rafeng/bakery/improve/business/controller/RecipeControllerTest.kt
+++ src/test/kotlin/com/rafeng/bakery/improve/business/controller/RecipeControllerTest.kt
@@ -1,32 +1,33 @@
 package com.rafeng.bakery.improve.business.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.rafeng.bakery.improve.business.model.dto.Recipe
+import com.rafeng.bakery.improve.business.service.impl.RecipeService
+import com.rafeng.bakery.improve.business.util.createRecipe
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
-@ActiveProfiles("h2")
 @WebMvcTest(RecipeController::class)
 class RecipeControllerTest @Autowired constructor(private val mockMvc: MockMvc) {
 
+    @MockBean
+    lateinit var recipeService: RecipeService
+
     @Test
     fun `Happy pass - create a new recipe in the system`() {
+        val recipe = createRecipe()
+        Mockito.doReturn(recipe).`when`(recipeService).save(recipe)
         mockMvc.perform(
             post("/api/v1/recipe")
                 .content(
                     ObjectMapper().writeValueAsString(
-                        Recipe(
-                            name = "Bun",
-                            description = "The best bun in the world",
-                            expirationPeriod = 1,
-                            cookingTime = 2.0
-                        )
+                        recipe
                     )
                 ).contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
